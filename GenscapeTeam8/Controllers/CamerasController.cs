e7aabeccbde60ec2a32dd84d59eacc733175c4a9
wimplash <rdransfo@gmail.com>
2014-03-20 06:47:13 -0400

    Adding CamerasController.cs

---
+++ GenscapeTeam8/Controllers/CamerasController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AForgeHack.Database;
+
+namespace GenscapeTeam8.Controllers
+{
+    public class CamerasController : Controller
+    {
+        private HackEntities context = new HackEntities();
+
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                ViewBag.Cameras = context.Cameras.ToList();
+            }
+            else
+            {
+                ViewBag.Cameras = context.Cameras.Where(camera => camera.Facility.FacilityID == id).ToList();
+            }
+            return View();
+        }
+	}
+}
