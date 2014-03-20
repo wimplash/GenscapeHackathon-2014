6444dd10665011efd6cbc324aad9d8e155f25a97
wimplash <rdransfo@gmail.com>
2014-03-19 22:14:27 -0400

    initial commit of api/images post handler

---
+++ GenscapeTeam8/Controllers/ImagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GenscapeTeam8.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        // POST api/images
+        public HttpResponseMessage Post(HttpRequestMessage request)
+        {
+            return request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
