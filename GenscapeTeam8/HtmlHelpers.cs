a91da5996d5ef49ae83dc76576538fee7d0c8e98
wimplash <rdransfo@gmail.com>
2014-03-19 23:33:12 -0400

    Yeah, so HtmlHelpers needed to be a static class

--- GenscapeTeam8/HtmlHelpers.cs
+++ GenscapeTeam8/HtmlHelpers.cs
@@ -8,24 +8,24 @@ using System.Web.Mvc.Html;
 
 namespace GenscapeTeam8
 {
-    public class HtmlHelpers
+    public static class HtmlHelpers
     {
-	    public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
-	    {
-		    var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
-		    var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
- 
-		    var builder = new TagBuilder("li")
-			{
-			    InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
-			};
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
+        {
+            var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+
+            var builder = new TagBuilder("li")
+            {
+                InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
+            };
 
             if (controllerName == currentController && actionName == currentAction)
             {
                 builder.AddCssClass("active");
             }
- 
-		    return new MvcHtmlString(builder.ToString());
-	    }
+
+            return new MvcHtmlString(builder.ToString());
+        }
     }
 }
