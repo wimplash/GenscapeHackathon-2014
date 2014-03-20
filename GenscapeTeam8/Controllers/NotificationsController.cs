31d39a8d4bde58171b1f4d1ef4af2d2b36ef261b
wimplash <rdransfo@gmail.com>
2014-03-19 22:44:23 -0400

    removing some other stuff and adding new controllers

---
+++ GenscapeTeam8/Controllers/NotificationsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenscapeTeam8.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        //
+        // GET: /Notifications/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ GenscapeTeam8/Controllers/SourcesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenscapeTeam8.Controllers
+{
+    public class SourcesController : Controller
+    {
+        //
+        // GET: /Sources/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
