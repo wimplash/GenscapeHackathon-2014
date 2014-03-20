bfcaf17ecce1062d43eb119b803a6221d886d442
wimplash <rdransfo@gmail.com>
2014-03-20 01:50:53 -0400

    Adding some more code around getting images and image parameters

---
+++ GenscapeTeam8/Controllers/ImageParametersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenscapeTeam8.Controllers
+{
+    public class ImageParametersController : Controller
+    {
+
+        public ActionResult GetImageParameters(int id)
+        {
+            return View();
+        }
+
+        // GET: /ImageParameters/
+        public ActionResult Index()
+        {
+            ViewBag.ImageParametersList = new List<Object>();
+            return View();
+        }
+	}
+}
--- GenscapeTeam8/Controllers/ImagesController.cs
+++ GenscapeTeam8/Controllers/ImagesController.cs
@@ -10,6 +10,18 @@ namespace GenscapeTeam8.Controllers
 {
     public class ImagesController : ApiController
     {
+        //get first image for source
+        public HttpResponseMessage Get(HttpRequestMessage request, [FromUri] int id)
+        {
+            byte[] imgData = new byte[0];
+            MemoryStream ms = new MemoryStream(imgData);
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
+            return response;
+        }
+
+        //get image for source for timestamp
         public HttpResponseMessage Get(HttpRequestMessage request, [FromUri] int id, [FromUri] long timestamp)
         {
             byte[] imgData = new byte[0];
