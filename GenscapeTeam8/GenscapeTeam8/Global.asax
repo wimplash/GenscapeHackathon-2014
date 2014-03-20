fc3db8570672e5562003e2a5f0dddaeb49ed4f03
wimplash <rdransfo@gmail.com>
2014-03-19 21:32:01 -0400

    initial commit

---
+++ GenscapeTeam8/GenscapeTeam8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GenscapeTeam8.MvcApplication" Language="C#" %>
---
+++ GenscapeTeam8/GenscapeTeam8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GenscapeTeam8.Startup))]
+namespace GenscapeTeam8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
